3c74e822cfb6977abfd0ce67a3965824929f3128
Eric Lin <eric@ericflin.com>
2023-03-21 10:35:47 -0700

    Debugging req.headers in azure functions

--- IpAddrLatLong/index.ts
+++ IpAddrLatLong/index.ts
@@ -10,8 +10,17 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
     };
     return;
   }
+  console.log(JSON.stringify(req.headers));
+  const ipAddr = req.headers['x-forwarded-for']?.split(':')[0];
+  if (!ipAddr || !/^(\d{1,3}\.){3}\d{1,3}$/.test(ipAddr)) {
+    context.res = {
+      status: 404
+    };
+    return;
+  }
+
   const client = new WebServiceClient('842331', process.env.MAXMIND_GEOLITE_KEY, { host: 'geolite.info' });
-  const response = await client.city(req.headers['x-forwarded-for'])
+  const response = await client.city(ipAddr)
   const body = {
     lat: response.location?.latitude,
     lng: response.location?.longitude,
